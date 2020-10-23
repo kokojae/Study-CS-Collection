99d85f5bc843d197da8365f52d3721d89452ed56
kokojae <54296776+kokojae@users.noreply.github.com>
2020-10-23 11:24:10 +0900

    반복자 예제코드
    yield 코드를 이용한 반복자 예제코드이다.
    아직 yield 구문을 완벽히 이해하지 못해서 어려운 부분이 있었지만 이 예제를 보며 yield를 조금 더 이해 할 수 있었다.

---
+++ Iterators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListEvenNumbers();
+        }
+
+        private static void ListEvenNumbers()
+        {
+            foreach (int number in EvenSequence(5,18))
+            {
+                Console.Write(number.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static IEnumerable<int> EvenSequence(
+            int firstNumber, int lastNumber)
+        {
+            // 범위에서 짝수를 산출하십시오.
+            for (var number = firstNumber; number <= lastNumber; number++)
+            {
+                if (number % 2 == 0)
+                {
+                    yield return number;
+                }
+            }
+        }
+    }
+}
